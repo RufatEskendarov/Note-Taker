1f01226a7eb420c2cc08e8b225d05f4f25442341
Rufat Eskendarov <rufat.eskendarov@gmail.com>
2023-01-23 22:51:32 -0400

    Updated middleware

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const path = require("path");
 const notesDb = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
