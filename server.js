ba20cfa60d61acc1ef3c2a7bb475690fdc451c0e
Rufat Eskendarov <rufat.eskendarov@gmail.com>
2023-01-23 23:45:36 -0400

    Updated DELETE method

--- server.js
+++ server.js
@@ -38,25 +38,32 @@ app.post("/api/notes", (req, res) => {
   res.json(saveNote);
 });
 
-
-
-
-
-fs.writeFileSync(jsonFilePath, JSON.stringify(notesDb), function (err) {
-  if (err) {
-    return console.log(err);
-  } else {
-    console.log("HOO-RAY, YOUR NOTE WAS DELETED!");
+// Delete a selected note
+app.delete("/api/notes/:id", (req, res) => {
+  console.log(req.params.id);
+  let jsonFilePath = path.join(__dirname, "/db/db.json");
+  // request to delete note by id.
+  for (let i = 0; i < notesDb.length; i++) {
+    if (notesDb[i].id == req.params.id) {
+      // Splice takes i position, and then deletes the 1 note.
+      notesDb.splice(i, 1);
+      break;
+    }
   }
-});
-res.json(notesDb);
+  fs.writeFileSync(jsonFilePath, JSON.stringify(notesDb), function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("HOO-RAY, YOUR NOTE WAS DELETED!");
+    }
+  });
+  res.json(notesDb);
 });
 
 // Get index.html file. Because of the Asterisk, this Get function must come last.
 app.get("*", (req, res) => {
-res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
 
 // Run the server
 app.listen(PORT, () => {
