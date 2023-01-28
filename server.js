f81f030dca3d7326d0d0be3e9e42c8d6e74add59
Rufat Eskendarov <rufat.eskendarov@gmail.com>
2023-01-27 22:42:15 -0400

    Errors correction in delet method

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const notesDb = require("./db/db.json");
+const notesDb = require("./Develop/db/db.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //middlewares
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.static("Develop/public"));
 
 // GET routes
 app.get("/api/notes", (req, res) => {
@@ -18,7 +18,7 @@ app.get("/api/notes", (req, res) => {
 
 // Get notes.html file
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 // POST routes adds notes to json file
@@ -40,8 +40,8 @@ app.post("/api/notes", (req, res) => {
 
 // Delete a selected note
 app.delete("/api/notes/:id", (req, res) => {
-  console.log(req.params.id);
-  let jsonFilePath = path.join(__dirname, "/db/db.json");
+  console.log("Note was deleted!");
+  let jsonFilePath = path.join(__dirname, "./Develop/db/db.json");
   // request to delete note by id.
   for (let i = 0; i < notesDb.length; i++) {
     if (notesDb[i].id == req.params.id) {
@@ -62,7 +62,7 @@ app.delete("/api/notes/:id", (req, res) => {
 
 // Get index.html file. Because of the Asterisk, this Get function must come last.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 // Run the server
@@ -73,7 +73,7 @@ app.listen(PORT, () => {
 // Write to database function
 const writeToDataBase = (notesDb) => {
   notesDb = JSON.stringify(notesDb);
-  fs.writeFile("./db/db.json", notesDb, (error) => {
+  fs.writeFile("./Develop/db/db.json", notesDb, (error) => {
     if (error) {
       return console.log(error);
     } else {
